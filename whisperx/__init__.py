fa80b78959d1c85c7fb2ce322ae6396fe12d0be6
Leo <leo.sun@berkeley.edu>
2024-03-06 12:16:03 -0800

    import fix

--- whisperx/__init__.py
+++ whisperx/__init__.py
@@ -2,4 +2,4 @@ from .transcribe import load_model
 from .alignment import load_align_model, align
 from .audio import load_audio
 from .vad import load_vad_model
-import segment
+from .segment import Segment
