cb7fbccba6909d03d7bec68ec9ef370931e54eaa
Leo <leo.sun@berkeley.edu>
2024-03-08 10:45:57 -0800

    testing

--- whisperx/__init__.py
+++ whisperx/__init__.py
@@ -1,5 +1,5 @@
-from .asr import load_model
-from .alignment import load_align_model, align
-from .audio import load_audio
-from .vad import load_vad_model
-from .segment import Segment
+# from .asr import load_model
+# from .alignment import load_align_model, align
+# from .audio import load_audio
+# from .vad import load_vad_model
+# from .segment import Segment
